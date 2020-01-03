2113b0dadc80b7c4772c82ca6d670ff04a997129
Christian Benz <christian.benz@uni-weimar.de>
2020-01-03 14:46:43 +0100

    Refactored inference

--- inference.py
+++ inference.py
@@ -14,27 +14,23 @@ import torch.nn.functional as F
 from TernausNet.unet_models import UNet16
 
 class CrackDetector():
-  def __init__(self, model_path='', image_path='', out_path='./results/', device='cuda:0'):
+  def __init__(self, model_path, device='cuda:0'):
     self.module_name = 'crack detector'
-    self.model_path = model_path
-    self.image_path = image_path
-    self.out_path = out_path
     self.device = torch.device(device)
-
-
-  def run(self):
+    
     # load model
-    model = UNet16(pretrained=True, num_classes=3, is_deconv=True)
-    checkpoint = torch.load(self.model_path)
-    model.load_state_dict(checkpoint['model'])
+    self.model = UNet16(pretrained=True, num_classes=3, is_deconv=True)
+    checkpoint = torch.load(model_path)
+    self.model.load_state_dict(checkpoint['model'])
 
     # move to device (GPU) and set mode to evaluation
-    model.to(self.device)
-#    model.cuda()
-    model.eval()
+    self.model.to(self.device)
+    self.model.eval()
 
+
+  def run(self, image_path, out_path):
     # load image
-    img = cv2.imread(self.image_path, cv2.IMREAD_COLOR)
+    img = cv2.imread(image_path, cv2.IMREAD_COLOR)
     h_orig, w_orig, _ = img.shape
 
     # make divisible by 32 to use fully convolutional property
@@ -57,7 +53,7 @@ class CrackDetector():
     X = Variable(X.unsqueeze(0)).to(self.device)  # [N, C, H, W]
 
     # compute forward pass
-    mask = model(X)
+    mask = self.model(X)
    
     # apply softmax on output
     mask = F.softmax(mask, dim=1).data.cpu().numpy()
@@ -69,12 +65,12 @@ class CrackDetector():
     mask = cv2.resize(mask, (w_orig, h_orig), cv2.INTER_AREA)
 
     # save heatmap
-    cv2.imwrite(self.out_path, mask*255)
+    cv2.imwrite(out_path, mask*255)
 
     # save overlayed image (225/2 for nicer use of color map)
     mask = np.uint8(cv2.cvtColor(mask, cv2.COLOR_GRAY2BGR) * 255/2)
     mask = cv2.applyColorMap(mask, cv2.COLORMAP_HOT)
-    cv2.imwrite(self.out_path.replace('.jpg', '_overlay.jpg'), cv2.addWeighted(img, 0.9, mask, 0.8, 0))
+    cv2.imwrite(out_path.replace('.jpg', '_overlay.jpg'), cv2.addWeighted(img, 0.9, mask, 0.8, 0))
     
 
 
@@ -86,7 +82,7 @@ if __name__ == "__main__":
   parser.add_argument('-device', type=str, default='cuda:0', required=False, help='device to used for inference')
   args = parser.parse_args()
 
-  detector = CrackDetector(args.model_path, args.image_path, args.out_path, args.device)
-  detector.run()
+  detector = CrackDetector(args.model_path, args.device)
+  detector.run(args.image_path, args.out_path)
 
 
--- run_inference.sh
+++ run_inference.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
-python inference.py -model_path ./models/model_weights_cracknausnet.pt -image_path ./uav75/test_img/DSC00595.jpg -out_path ./results/DSC00595.jpg -device cuda:0
+python inference.py \
+-model_path ./models/model_weights_cracknausnet.pt \
+-image_path ./uav75/test_img/DSC00595.jpg \
+-out_path ./results/DSC00595.jpg \
+-device cuda:0
