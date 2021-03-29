77796a386625f2cd0b8977f4271bb6a23751cc5a
DennisBohm1 <d.bohm@student.tudelft.nl>
2021-03-29 15:57:36 +0200

    Update write

--- write_model_to_file.py
+++ write_model_to_file.py
@@ -1,6 +1,13 @@
 import torch
+import json
+import sys
 
-with open('models/model_best.pt', 'rb') as content_file:
-    content = content_file.read()
+original_stdout = sys.stdout # Save a reference to the original standard output
+
 with open('models/model_best.txt', 'w') as target_file:
-    target_file.write(content)
+    sys.stdout = target_file # Change the standard output to the file we created.
+    print(torch.load('models/model_best.pt',  map_location=torch.device('cpu')))
+    sys.stdout = original_stdout # Reset the standard output to its original value
+
+
+    # target_file.write(torch.load('models/model_best.pt',  map_location=torch.device('cpu')))
