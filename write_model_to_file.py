ddaf9c7227414635f20bec1b94ec7470cbc98660
DennisBohm1 <d.bohm@student.tudelft.nl>
2021-03-29 15:10:38 +0200

    Update write script

--- write_model_to_file.py
+++ write_model_to_file.py
@@ -1,24 +1,26 @@
 import torch
 import io
 
-torch.jit.load('model_best.pt')
+filename: str = "models/model_best.pt"
+
+torch.load(filename)
 
 # Load ScriptModule from io.BytesIO object
-with open('model_best.pt', 'rb') as f:
+with open(filename, 'rb') as f:
     buffer = io.BytesIO(f.read())
 
 # Load all tensors to the original device
-torch.jit.load(buffer)
+torch.load(buffer)
 
 # Load all tensors onto CPU, using a device
 buffer.seek(0)
-torch.jit.load(buffer, map_location=torch.device('cpu'))
+torch.load(buffer, map_location=torch.device('cpu'))
 
 # Load all tensors onto CPU, using a string
 buffer.seek(0)
-torch.jit.load(buffer, map_location='cpu')
+torch.load(buffer, map_location='cpu')
 
 # Load with extra files.
 extra_files = {'foo.txt': ''}  # values will be replaced with data
-torch.jit.load('model_best.pt', _extra_files=extra_files)
+torch.load(filename, _extra_files=extra_files)
 print(extra_files['foo.txt'])
