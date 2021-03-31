74fbf1eb28c1385ef2ba2f5491b8a52ece15e281
DennisBohm1 <d.bohm@student.tudelft.nl>
2021-03-31 15:17:06 +0200

    update import

--- train_unet.py
+++ train_unet.py
@@ -6,7 +6,7 @@ import torchvision.transforms as transforms
 from torch.utils.data import DataLoader, Dataset, random_split
 import torch.nn.functional as F
 from torch.autograd import Variable
-import DiceLoss from dice_loss
+from dice_loss import DiceLoss
 import shutil
 from data_loader import ImgDataSet
 import os
