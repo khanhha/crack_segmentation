0b103c60f9659c638bf1275ef9a5f2bf71d8e78e
DennisBohm1 <d.bohm@student.tudelft.nl>
2021-03-31 15:16:09 +0200

    edit diceloss

--- dice_loss.py
+++ dice_loss.py
@@ -4,7 +4,7 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
-from .one_hot import one_hot
+from torchgeometry.losses.one_hot import one_hot
 
 
 # based on:
@@ -53,24 +53,31 @@ class DiceLoss(nn.Module):
             self,
             input: torch.Tensor,
             target: torch.Tensor) -> torch.Tensor:
+        torch.reshape(input, (-1, 100, 448, 448))
         if not torch.is_tensor(input):
             raise TypeError("Input type is not a torch.Tensor. Got {}"
                             .format(type(input)))
+        if not len(input.shape) == 4:
+            raise ValueError("Invalid input shape, we expect BxNxHxW. Got: {}"
+                             .format(input.shape))
+        if not input.shape[-2:] == target.shape[-2:]:
+            raise ValueError("input and target shapes must be the same. Got: {}"
+                             .format(input.shape, input.shape))
         if not input.device == target.device:
             raise ValueError(
-                "input and target must be in the same device. Got: {}".format(
+                "input and target must be in the same device. Got: {}" .format(
                     input.device, target.device))
-
         # compute softmax over the classes axis
-        input_soft = F.softmax(input)
+        input_soft = F.softmax(input, dim=1)
 
         # create the labels one hot tensor
-        target_one_hot = one_hot(target, num_classes=input.shape,
+        target_one_hot = one_hot(target, num_classes=input.shape[1],
                                  device=input.device, dtype=input.dtype)
 
         # compute the actual dice score
-        intersection = torch.sum(input_soft * target_one_hot)
-        cardinality = torch.sum(input_soft + target_one_hot)
+        dims = (1, 2, 3)
+        intersection = torch.sum(input_soft * target_one_hot, dims)
+        cardinality = torch.sum(input_soft + target_one_hot, dims)
 
         dice_score = 2. * intersection / (cardinality + self.eps)
         return torch.mean(1. - dice_score)
