f5a0f71729f4206cba7741746322cc3828f9f5ce
GCLOUD instance-3 <d.bohm@student.tudelft.nl>
2021-04-05 15:58:05 +0000

    Comment everything that doesn't work

--- evaluate_unet.py
+++ evaluate_unet.py
@@ -50,16 +50,16 @@ def f1_loss(y_true: torch.Tensor, y_pred: torch.Tensor, is_training=False) -> to
     - https://discuss.pytorch.org/t/calculating-precision-recall-and-f1-score-in-case-of-multi-label-classification/28265/6
 
     '''
-    assert y_true.ndim == 1
-    assert y_pred.ndim == 1 or y_pred.ndim == 2
+    # assert y_true.ndim == 1
+    # assert y_pred.ndim == 1 or y_pred.ndim == 2
 
-    if y_pred.ndim == 2:
-        y_pred = y_pred.argmax(dim=1)
+    # if y_pred.ndim == 2:
+    #    y_pred = y_pred.argmax(dim=1)
 
-    tp = (y_true * y_pred).sum().to(torch.float32)
-    tn = ((1 - y_true) * (1 - y_pred)).sum().to(torch.float32)
-    fp = ((1 - y_true) * y_pred).sum().to(torch.float32)
-    fn = (y_true * (1 - y_pred)).sum().to(torch.float32)
+    tp = (y_true * y_pred).sum()#.to(torch.float32)
+    tn = ((1 - y_true) * (1 - y_pred)).sum()#.to(torch.float32)
+    fp = ((1 - y_true) * y_pred).sum()#.to(torch.float32)
+    fn = (y_true * (1 - y_pred)).sum()#.to(torch.float32)
 
     epsilon = 1e-8
 
@@ -67,7 +67,7 @@ def f1_loss(y_true: torch.Tensor, y_pred: torch.Tensor, is_training=False) -> to
     recall = tp / (tp + fn + epsilon)
 
     f1 = 2 * (precision * recall) / (precision + recall + epsilon)
-    f1.requires_grad = is_training
+    # f1.requires_grad = is_training
     return f1
 
 def recall(y_true: torch.Tensor, y_pred: torch.Tensor) -> torch.Tensor:
