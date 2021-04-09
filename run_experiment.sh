683fcd9f1a744c592a65ca33a790e3e02895a7d3
DennisBohm1 <d.bohm@student.tudelft.nl>
2021-04-09 23:03:10 +0200

    Just run cfd

--- run_experiment.sh
+++ run_experiment.sh
@@ -24,56 +24,56 @@
 
 # LR Rates: 0.001, 0.005, 0.01, 0.015, 0.02, 0.05, 0.1, 0.2
 
-sudo rm ./Experiment/CRACK500/test_results/Crack*
-
-# Train Crack First Run (Log files are named as follows: Crack<decimals LR>First.log
-python train_unet.py -data_dir ./CRACK500/train -model_dir ./Experiment/CRACK500/model/first -model_type resnet34 -lr 0.001
-python inference_unet.py -img_dir ./CRACK500/test/images -model_path ./Experiment/CRACK500/model/first/model_best.pt -model_type resnet34 -out_pred_dir ./Experiment/CRACK500/test_results/first
-python evaluate_unet.py -ground_truth_dir ./CRACK500/test/masks -pred_dir ./Experiment/CRACK500/test_results/first > Experiment/CRACK500/test_results/Crack001First.log
-sudo rm -r ./Experiment/CRACK500/model/first/*
-sudo rm -r ./Experiment/CRACK500/test_results/first/*
-
-python train_unet.py -data_dir ./CRACK500/train -model_dir ./Experiment/CRACK500/model/first -model_type resnet34 -lr 0.005
-python inference_unet.py -img_dir ./CRACK500/test/images -model_path ./Experiment/CRACK500/model/first/model_best.pt -model_type resnet34 -out_pred_dir ./Experiment/CRACK500/test_results/first
-python evaluate_unet.py -ground_truth_dir ./CRACK500/test/masks -pred_dir ./Experiment/CRACK500/test_results/first > Experiment/CRACK500/test_results/Crack005First.log
-sudo rm -r ./Experiment/CRACK500/model/first/*
-sudo rm -r ./Experiment/CRACK500/test_results/first/*
-
-python train_unet.py -data_dir ./CRACK500/train -model_dir ./Experiment/CRACK500/model/first -model_type resnet34 -lr 0.01
-python inference_unet.py -img_dir ./CRACK500/test/images -model_path ./Experiment/CRACK500/model/first/model_best.pt -model_type resnet34 -out_pred_dir ./Experiment/CRACK500/test_results/first
-python evaluate_unet.py -ground_truth_dir ./CRACK500/test/masks -pred_dir ./Experiment/CRACK500/test_results/first > Experiment/CRACK500/test_results/Crack01First.log
-sudo rm -r ./Experiment/CRACK500/model/first/*
-sudo rm -r ./Experiment/CRACK500/test_results/first/*
-
-python train_unet.py -data_dir ./CRACK500/train -model_dir ./Experiment/CRACK500/model/first -model_type resnet34 -lr 0.015
-python inference_unet.py -img_dir ./CRACK500/test/images -model_path ./Experiment/CRACK500/model/first/model_best.pt -model_type resnet34 -out_pred_dir ./Experiment/CRACK500/test_results/first
-python evaluate_unet.py -ground_truth_dir ./CRACK500/test/masks -pred_dir ./Experiment/CRACK500/test_results/first > Experiment/CRACK500/test_results/Crack015First.log
-sudo rm -r ./Experiment/CRACK500/model/first/*
-sudo rm -r ./Experiment/CRACK500/test_results/first/*
-
-python train_unet.py -data_dir ./CRACK500/train -model_dir ./Experiment/CRACK500/model/first -model_type resnet34 -lr 0.02
-python inference_unet.py -img_dir ./CRACK500/test/images -model_path ./Experiment/CRACK500/model/first/model_best.pt -model_type resnet34 -out_pred_dir ./Experiment/CRACK500/test_results/first
-python evaluate_unet.py -ground_truth_dir ./CRACK500/test/masks -pred_dir ./Experiment/CRACK500/test_results/first > Experiment/CRACK500/test_results/Crack02First.log
-sudo rm -r ./Experiment/CRACK500/model/first/*
-sudo rm -r ./Experiment/CRACK500/test_results/first/*
-
-python train_unet.py -data_dir ./CRACK500/train -model_dir ./Experiment/CRACK500/model/first -model_type resnet34 -lr 0.05
-python inference_unet.py -img_dir ./CRACK500/test/images -model_path ./Experiment/CRACK500/model/first/model_best.pt -model_type resnet34 -out_pred_dir ./Experiment/CRACK500/test_results/first
-python evaluate_unet.py -ground_truth_dir ./CRACK500/test/masks -pred_dir ./Experiment/CRACK500/test_results/first > Experiment/CRACK500/test_results/Crack05First.log
-sudo rm -r ./Experiment/CRACK500/model/first/*
-sudo rm -r ./Experiment/CRACK500/test_results/first/*
-
-python train_unet.py -data_dir ./CRACK500/train -model_dir ./Experiment/CRACK500/model/first -model_type resnet34 -lr 0.1
-python inference_unet.py -img_dir ./CRACK500/test/images -model_path ./Experiment/CRACK500/model/first/model_best.pt -model_type resnet34 -out_pred_dir ./Experiment/CRACK500/test_results/first
-python evaluate_unet.py -ground_truth_dir ./CRACK500/test/masks -pred_dir ./Experiment/CRACK500/test_results/first > Experiment/CRACK500/test_results/Crack1First.log
-sudo rm -r ./Experiment/CRACK500/model/first/*
-sudo rm -r ./Experiment/CRACK500/test_results/first/*
-
-python train_unet.py -data_dir ./CRACK500/train -model_dir ./Experiment/CRACK500/model/first -model_type resnet34 -lr 0.2
-python inference_unet.py -img_dir ./CRACK500/test/images -model_path ./Experiment/CRACK500/model/first/model_best.pt -model_type resnet34 -out_pred_dir ./Experiment/CRACK500/test_results/first
-python evaluate_unet.py -ground_truth_dir ./CRACK500/test/masks -pred_dir ./Experiment/CRACK500/test_results/first > Experiment/CRACK500/test_results/Crack2First.log
-sudo rm -r ./Experiment/CRACK500/model/first/*
-sudo rm -r ./Experiment/CRACK500/test_results/first/*
+#sudo rm ./Experiment/CRACK500/test_results/Crack*
+#
+## Train Crack First Run (Log files are named as follows: Crack<decimals LR>First.log
+#python train_unet.py -data_dir ./CRACK500/train -model_dir ./Experiment/CRACK500/model/first -model_type resnet34 -lr 0.001
+#python inference_unet.py -img_dir ./CRACK500/test/images -model_path ./Experiment/CRACK500/model/first/model_best.pt -model_type resnet34 -out_pred_dir ./Experiment/CRACK500/test_results/first
+#python evaluate_unet.py -ground_truth_dir ./CRACK500/test/masks -pred_dir ./Experiment/CRACK500/test_results/first > Experiment/CRACK500/test_results/Crack001First.log
+#sudo rm -r ./Experiment/CRACK500/model/first/*
+#sudo rm -r ./Experiment/CRACK500/test_results/first/*
+#
+#python train_unet.py -data_dir ./CRACK500/train -model_dir ./Experiment/CRACK500/model/first -model_type resnet34 -lr 0.005
+#python inference_unet.py -img_dir ./CRACK500/test/images -model_path ./Experiment/CRACK500/model/first/model_best.pt -model_type resnet34 -out_pred_dir ./Experiment/CRACK500/test_results/first
+#python evaluate_unet.py -ground_truth_dir ./CRACK500/test/masks -pred_dir ./Experiment/CRACK500/test_results/first > Experiment/CRACK500/test_results/Crack005First.log
+#sudo rm -r ./Experiment/CRACK500/model/first/*
+#sudo rm -r ./Experiment/CRACK500/test_results/first/*
+#
+#python train_unet.py -data_dir ./CRACK500/train -model_dir ./Experiment/CRACK500/model/first -model_type resnet34 -lr 0.01
+#python inference_unet.py -img_dir ./CRACK500/test/images -model_path ./Experiment/CRACK500/model/first/model_best.pt -model_type resnet34 -out_pred_dir ./Experiment/CRACK500/test_results/first
+#python evaluate_unet.py -ground_truth_dir ./CRACK500/test/masks -pred_dir ./Experiment/CRACK500/test_results/first > Experiment/CRACK500/test_results/Crack01First.log
+#sudo rm -r ./Experiment/CRACK500/model/first/*
+#sudo rm -r ./Experiment/CRACK500/test_results/first/*
+#
+#python train_unet.py -data_dir ./CRACK500/train -model_dir ./Experiment/CRACK500/model/first -model_type resnet34 -lr 0.015
+#python inference_unet.py -img_dir ./CRACK500/test/images -model_path ./Experiment/CRACK500/model/first/model_best.pt -model_type resnet34 -out_pred_dir ./Experiment/CRACK500/test_results/first
+#python evaluate_unet.py -ground_truth_dir ./CRACK500/test/masks -pred_dir ./Experiment/CRACK500/test_results/first > Experiment/CRACK500/test_results/Crack015First.log
+#sudo rm -r ./Experiment/CRACK500/model/first/*
+#sudo rm -r ./Experiment/CRACK500/test_results/first/*
+#
+#python train_unet.py -data_dir ./CRACK500/train -model_dir ./Experiment/CRACK500/model/first -model_type resnet34 -lr 0.02
+#python inference_unet.py -img_dir ./CRACK500/test/images -model_path ./Experiment/CRACK500/model/first/model_best.pt -model_type resnet34 -out_pred_dir ./Experiment/CRACK500/test_results/first
+#python evaluate_unet.py -ground_truth_dir ./CRACK500/test/masks -pred_dir ./Experiment/CRACK500/test_results/first > Experiment/CRACK500/test_results/Crack02First.log
+#sudo rm -r ./Experiment/CRACK500/model/first/*
+#sudo rm -r ./Experiment/CRACK500/test_results/first/*
+#
+#python train_unet.py -data_dir ./CRACK500/train -model_dir ./Experiment/CRACK500/model/first -model_type resnet34 -lr 0.05
+#python inference_unet.py -img_dir ./CRACK500/test/images -model_path ./Experiment/CRACK500/model/first/model_best.pt -model_type resnet34 -out_pred_dir ./Experiment/CRACK500/test_results/first
+#python evaluate_unet.py -ground_truth_dir ./CRACK500/test/masks -pred_dir ./Experiment/CRACK500/test_results/first > Experiment/CRACK500/test_results/Crack05First.log
+#sudo rm -r ./Experiment/CRACK500/model/first/*
+#sudo rm -r ./Experiment/CRACK500/test_results/first/*
+#
+#python train_unet.py -data_dir ./CRACK500/train -model_dir ./Experiment/CRACK500/model/first -model_type resnet34 -lr 0.1
+#python inference_unet.py -img_dir ./CRACK500/test/images -model_path ./Experiment/CRACK500/model/first/model_best.pt -model_type resnet34 -out_pred_dir ./Experiment/CRACK500/test_results/first
+#python evaluate_unet.py -ground_truth_dir ./CRACK500/test/masks -pred_dir ./Experiment/CRACK500/test_results/first > Experiment/CRACK500/test_results/Crack1First.log
+#sudo rm -r ./Experiment/CRACK500/model/first/*
+#sudo rm -r ./Experiment/CRACK500/test_results/first/*
+#
+#python train_unet.py -data_dir ./CRACK500/train -model_dir ./Experiment/CRACK500/model/first -model_type resnet34 -lr 0.2
+#python inference_unet.py -img_dir ./CRACK500/test/images -model_path ./Experiment/CRACK500/model/first/model_best.pt -model_type resnet34 -out_pred_dir ./Experiment/CRACK500/test_results/first
+#python evaluate_unet.py -ground_truth_dir ./CRACK500/test/masks -pred_dir ./Experiment/CRACK500/test_results/first > Experiment/CRACK500/test_results/Crack2First.log
+#sudo rm -r ./Experiment/CRACK500/model/first/*
+#sudo rm -r ./Experiment/CRACK500/test_results/first/*
 
 # LR Rates: 0.001, 0.005, 0.01, 0.015, 0.02, 0.05, 0.1, 0.2
 
